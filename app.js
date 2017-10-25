44728885037fd2aad0d1e492d9849b8748fc08e3
Gabriel Downs <gndowns@gmail.com>
2017-10-25 14:30:45 -0400

    add images and route for minecraft circuits page

--- app.js
+++ app.js
@@ -4,11 +4,23 @@ const ejs = require('ejs');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// use public/ dir
+app.use(express.static(__dirname + '/public'));
+
+// use views dir
 app.set('views', __dirname + '/views');
+
+// render raw html
 app.engine('html', ejs.renderFile);
 
 app.get('/', function(req, res) {
   res.render('index.html');
+});
+
+
+// minecraft digital circuits
+app.get('/minecraft', function(req, res) {
+  res.render('minecraft.html');
 });
 
 app.listen(PORT, function() {
